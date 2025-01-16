df66e146061481ec7ff87f67588595b630696ced
이니 <38304918+JISUSAMA@users.noreply.github.com>
2025-01-16 19:28:57 +0900

    11

--- linkup/src/main/java/com/bugisu/linkup/LinkupApplication.java
+++ linkup/src/main/java/com/bugisu/linkup/LinkupApplication.java
@@ -2,12 +2,17 @@ package com.bugisu.linkup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing //BasicEntity를 사용하기 위함.
+@EnableJpaRepositories(basePackages = "com.bugisu.linkup.repository")
 public class LinkupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinkupApplication.class, args);
+		System.out.println("API server started...");
 	}
 
 }
